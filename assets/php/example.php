2ffdd6b79af867c23024341e84cdabb4c5d2a042
Thanawit Kaniwaranon <tunnie0409.tk@gmail.com>
2023-05-02 17:02:58 +0700

    smtp

---
+++ assets/php/example.php
@@ -0,0 +1,28 @@
+<?php
+		$mailto = "yourmail@gmail@gmail.com";
+		$mailSub = "Lorem Ipsum คือ เนื้อหาจำลองแบบเรียบๆ";
+		$mailMsg = "
+			Lorem Ipsum คือ เนื้อหาจำลองแบบเรียบๆ ที่ใช้กันในธุรกิจงานพิมพ์หรืองานเรียงพิมพ์ มันได้กลายมาเป็นเนื้อหาจำลองมาตรฐานของธุรกิจดังกล่าวมาตั้งแต่ศตวรรษที่
+		";
+	 require 'PHPMailer/PHPMailerAutoload.php';
+	 $mail = new PHPMailer();
+	 $mail ->IsSmtp();
+		
+	 $mail ->SMTPAuth = true;
+	 $mail ->SMTPSecure = 'tls';
+	 $mail ->Host = "smtp.gmail.com";
+	 $mail ->Port = 587; // or 587
+	 //$mail ->IsHTML(true);
+	 $mail ->Username = "yourmail@gmail.com";
+	 $mail ->Password = "email acount Password";
+	 $mail ->SetFrom("yourmail@gmail.com", "Company name");
+	 $mail ->Subject = $mailSub;
+	 $mail ->Body = $mailMsg;
+	 $mail ->AddAddress($mailto);
+
+	 if(!$mail->Send()){
+			 echo "Mail Not Sent";
+	 }
+	 else{
+			 echo "Mail Sent";
+	 }
--- assets/php/mail.php
+++ assets/php/mail.php
@@ -1,13 +1,29 @@
 <?php
 
-$to 		= 'kt.thanawit@gmail.com';
-$headers	= 'FROM: "'.$email.'"';
-
-//All form values
+$mailto = "kt.thanawit@gmail.com";
+$mailSub = $_POST['subject'];
+$mailMsg = $_POST['msg'];
 $name 		= $_POST['name'];
 $email 		= $_POST['email'];
-$subject 	= $_POST['subject'];
-$msg 		= $_POST['msg'];
-$output 	= "Name: ".$name."\nEmail: ".$email."\nSubject: ".$subject."\n\nMessage: ".$msg;
 
-$send		= mail($to, $name, $output, $headers);
+require './PHPMailer/PHPMailerAutoload.php';
+$mail = new PHPMailer();
+$mail->IsSmtp();
+
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'tls';
+$mail->Host = "smtp.gmail.com";
+$mail->Port = 587; // or 587
+//$mail ->IsHTML(true);
+$mail->Username = "testlc.cw@gmail.com";
+$mail->Password = "aaahldgeuvcgauca";
+$mail->SetFrom($email, $name);
+$mail->Subject = $mailSub;
+$mail->Body = $mailMsg;
+$mail->AddAddress($mailto);
+
+if (!$mail->Send()) {
+    echo "Mail Not Sent";
+} else {
+    echo "Mail Sent";
+}
